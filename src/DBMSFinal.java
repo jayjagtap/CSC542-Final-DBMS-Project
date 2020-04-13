19da63951e86e2b8df3e791f0a0cf0dc3eb677e2
NischalKash <nischalkashyap.adm2019@gmail.com>
2020-04-13 15:55:26 -0400

    4PM

--- src/DBMSFinal.java
+++ src/DBMSFinal.java
@@ -19,10 +19,280 @@ public class DBMSFinal {
 		} catch (Exception e) {
 			System.out.println("Driver missing!");
 		}
-		
 		runManager();
+		runEditor();
+		runAuthor();
 	}
 	
+	public static void runAuthor(){
+		System.out.println("");
+		System.out.println("");
+		System.out.println("***********************");
+		System.out.println("You are in Authors View");
+		System.out.println("***********************");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("The following are the operations that can be performed by the Editor");
+		System.out.println("Please select the number correspondingly");
+		System.out.println("");
+		System.out.println("");
+		int choice;;
+		API obj = new API();
+
+		do{
+			System.out.println("1. Update Books");
+			System.out.println("2. Add Chapters");
+			System.out.println("3. Total Chapters for a Particular Book");
+			System.out.println("4. Update Chapter Texts");
+			System.out.println("5. Display Publication");
+			System.out.println("6. Display Books");
+			System.out.println("7. Display Chapters");
+			System.out.println("8. Display EditBooks");
+
+			System.out.println("Please Enter the operation number to be performed");
+			choice = getMenuChoice();
+			switch (choice){
+				case 1 : try{
+					obj.update_books();
+				}catch (SQLException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+				}
+				break;
+			 case 2:
+				try {
+					obj.add_chapters();
+				}catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			 break;
+			 case 3:
+				 try {
+					obj.total_chapters();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 4:
+				 try {
+
+					obj.update_chapters_text();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 5:
+				 try {
+					obj.displayPublication();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 6:
+				 try {
+					obj.displayBooks();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 7:
+				 try {
+					obj.displayChapters();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 8:
+				 try {
+					obj.displayEditBooks();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			default: System.out.println("Invalid Entry, try Again :)"); 
+			break;
+		}
+	}while(choice!=0);
+}
+
+	public static void runEditor(){
+		System.out.println("***********************");
+		System.out.println("You are in Editors View");
+		System.out.println("***********************");
+
+		System.out.println("");
+		System.out.println("");
+		System.out.println("The following are the operations that can be performed by the Editor");
+		System.out.println("Please select the number correspondingly");
+		System.out.println("");
+		System.out.println("");
+		int choice;;
+		API obj = new API();
+
+		do{
+			System.out.println("1. Update Books");
+			System.out.println("2. Update Periodicals");
+			System.out.println("3. Update Publication");
+			System.out.println("4. Display Total Number of Chapters for an ISBN");
+			System.out.println("5. Display Total Number of Articles for an ISBN");
+			System.out.println("6. Display Total Number of Articles");
+			System.out.println("7. Update Article Texts");
+			System.out.println("8. Update Chapter Texts");
+			System.out.println("9. Delete an Article for an ISBN");
+			System.out.println("10. Display Periodicals");
+			System.out.println("11. Display Publication");
+			System.out.println("12. Display Articles");
+			System.out.println("13. Display Books");
+			System.out.println("14. Display Chapters");
+			System.out.println("15. Display EditBooks");
+			System.out.println("16. Display EditPeriodicals");
+			System.out.println("0. To Exit the Editor View");
+
+			System.out.println("Please Enter the operation number to be performed");
+			choice = getMenuChoice();
+			switch (choice){
+				case 1 : try{
+					obj.update_books();
+				}catch (SQLException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+				}
+				break;
+			 case 2:
+				try {
+					obj.update_periodicals();
+				}catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			 break;
+			 case 3:
+				 try {
+					obj.update_publication();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 4:
+				 try {
+					obj.total_chapters();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 5:
+				 try {
+					obj.total_articles();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 6:
+				 try {
+					obj.total_number_articles();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 7:
+				 try {
+					obj.update_articles_text();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 8:
+				 try {
+					obj.update_chapters_text();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+
+				 case 9:
+				 try {
+					obj.delete_article();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 10:
+				 try {
+					obj.displayPeriodicals();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 11:
+				 try {
+					obj.displayPublication();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 12:
+				 try {
+					obj.displayArticles();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 13:
+				 try {
+					obj.displayBooks();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 14:
+				 try {
+					obj.displayChapters();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 15:
+				 try {
+					obj.displayEditBooks();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+				 case 16:
+				 try {
+					obj.displayEditPeriodicals();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 default: System.out.println("Invalid Entry, try Again :)"); 
+			break;
+			 
+			}
+		}while(choice!=0);
+	}
+
 	public static void runManager() {
 		int choice;
 		API a = new API();
