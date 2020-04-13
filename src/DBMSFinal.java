722411b5369f7006114d85a8eb70a1dabfda0541
jayjagtap <jayjagtap.01@gmail.com>
2020-04-13 09:32:37 -0400

    DBMS FInal update

--- src/DBMSFinal.java
+++ src/DBMSFinal.java
@@ -31,11 +31,18 @@ public class DBMSFinal {
 			System.out.println("Welcome to Manager Section: Here are the APIS");
 			System.out.println("1. Add Staff");
 			System.out.println("2. Update Author");
-			System.out.println("Welcome to Manager Section: Here are the APIS");
-			System.out.println("Welcome to Manager Section: Here are the APIS");
-			System.out.println("Welcome to Manager Section: Here are the APIS");
-			System.out.println("Welcome to Manager Section: Here are the APIS");
-			System.out.println("Welcome to Manager Section: Here are the APIS");
+			System.out.println("3. Update Editor");
+			System.out.println("4. Update Journalist");
+			System.out.println("5. Update Staff");
+			System.out.println("6. Delete Staff");
+			System.out.println("7. Add Publications");
+			System.out.println("8. Update Publication");
+			System.out.println("2. Update Author");
+			System.out.println("3. Update Editor");
+			System.out.println("4. Update Journalist");
+			System.out.println("5. Update Staff");
+			System.out.println("6. Delete Staff");
+			System.out.println("0. Exit");
 			
 			
 			System.out.println("Please enter operation number to be performed");
@@ -48,19 +55,68 @@ public class DBMSFinal {
 					//e.printStackTrace();
 				}
 			 break;
-			 case 2: try {
-					API.update_author();
+			 case 2:
+				try {
+					a.update_author();
 				} catch (SQLException e) {
 					// TODO Auto-generated catch block
-					//e.printStackTrace();
+					e.printStackTrace();
 				}
 			 break;
+			 case 3:
+				 try {
+					a.update_editor();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 4:
+				 try {
+					a.update_journalist();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 5:
+				 try {
+					a.update_staff();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 6:
+				 try {
+					a.delete_staff();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 7:
+				 try {
+					a.add_publications();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
+			 case 8:
+				 try {
+					a.update_publication();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				 break;
 			 default: System.out.println("Invalid Entry, try Again :)"); 
-				 
+			break;
 			 	
 			 }
 	            
-		}while(false);
+		}while(choice != 0);
 	}
 	
 	public static int getMenuChoice() {
@@ -73,7 +129,7 @@ public class DBMSFinal {
             } catch (NumberFormatException e) {
                 System.out.println("Invalid selection. Numbers only please.");
             }
-        } while (choice < 0 || choice > 4);
+        } while (choice == -1);
         return choice;
     }
 	
